89d3f65e7901a7276e871d2895211fee0939246e
h8moss <danarmenta32@gmail.com>
2025-05-23 09:08:21 -0600

    FEAT: Added fs module to program.hh

--- include/common/program.hh
+++ include/common/program.hh
@@ -7,6 +7,13 @@ public:
   void main();
 
   // ----------- File System -----------
-
+  void touch(string path) override;
+  void mkdir(string path) override;
+  void cat(string path) override;
+  void ls(string path) override;
+  void write_file(string path, string contents, bool append) override;
+  void del(string path) override;
+  void del_content(string path, int l, bool lines) override;
+  void cd(string path) override;
   // -----------------------------------
 };
